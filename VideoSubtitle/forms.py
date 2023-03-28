b75bec313ff4df93d16b536d5869276be52829ce
Abhay Arora <AbhayArora@my.unt.edu>
2023-03-28 16:33:07 -0500

    redundant folders removed (related to other frameworks)

--- VideoSubtitle/forms.py
+++
@@ -1,9 +0,0 @@
-# from django.forms import ModelForm
-#
-# from VideoSubtitle.models import VideoSubtitle_video
-#
-#
-# class VideoForm(ModelForm):
-#     class Meta:
-#         model= VideoSubtitle_video
-#         fields= ["name", "videofile"]
